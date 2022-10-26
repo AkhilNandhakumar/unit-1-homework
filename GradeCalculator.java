bf81bdc89fa586863e3b04c39cc798a8bc275ed0
akhil <akhil@nandhakumar.net>
2022-10-26 11:32:04 -0700

    commit

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,36 +2,29 @@ import java.util.Scanner;
 
 public class GradeCalculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("The final is in a separate category. Please input true or false.");
-        boolean category = sc.nextBoolean();
-        System.out.println("What is your current grade?");
-        double currentGrade = sc.nextDouble();
-        System.out.println("What is your desired grade after the final?");
-        double desiredGrade = sc.nextDouble();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Is final in a separate category: (true/false)");
+        boolean check = input.nextBoolean();
 
-        if(category){
-            System.out.println("What percent of your grade is the final?");
-            int finalPercentage = sc.nextInt();
-            double finalGrade = (desiredGrade - (currentGrade * ((100-finalPercentage)/100))/(finalPercentage-100));
-            System.out.println("You need a " + finalGrade + "% on the final.");
-            sc.close();
+        if(check){
+            System.out.println("Current Grade:");
+            double currentGrade = input.nextDouble();
+
+            System.out.println("Desired Grade:");
+            double desiredGrade = input.nextDouble();
+
+            System.out.println("How much percent of your grade is the final:");
+            double finalPercent = input.nextDouble();
+            double finalGrade = ((desiredGrade-((1-finalPercent/100)*currentGrade))/(finalPercent/100));
+            System.out.println("You need: " + finalGrade + "%");
+            input.close();
         }
+
         else{
-            System.out.println("What percent of your grade is the test category?");
-            int testPercentage = sc.nextInt();
-            System.out.println("What is the current percentage you have in the test category?");
-            int currentTestPercentage = sc.nextInt();
-            System.out.println("How many points are currently in the test category?");
-            int currentTestPoints = sc.nextInt();
-            System.out.println("How many points are the final worth?");
-            int finalPoints = sc.nextInt();
-            double unchanged = currentGrade - currentTestPercentage;
-            double newTestPercentage = desiredGrade - unchanged;
-            double neededTotalPoints = (newTestPercentage/testPercentage) * (finalPoints + currentTestPoints);
-            double currentEarnedPoints = (currentTestPercentage/testPercentage) * currentTestPoints;
-            double neededFinalPoints = neededTotalPoints - currentEarnedPoints;
-            System.out.println("You need " + neededFinalPoints + "points on the final.");
+            // i don't know :(
+            input.close();
         }
+
     }
 }
+   
