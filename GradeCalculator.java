765be77637fcf957c26ad9520076bcca321dee32
akhil <akhil@nandhakumar.net>
2022-11-07 12:22:59 -0800

    calc

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -21,8 +21,32 @@ public class GradeCalculator {
         }
 
         else{
-            // i don't know :(
-            input.close();
+            System.out.print("Current grade? ");
+            double currentGrade = input.nextDouble();
+            System.out.println(currentGrade);
+
+            System.out.print("Test category weight: ");
+            int testCat = input.nextInt();
+            System.out.println(testCat);
+
+            System.out.print("total points in test category: ");
+            int testCatPoints = input.nextInt();
+            System.out.println(testCatPoints);
+
+            System.out.print("final points: ");
+            int finalPoints = input.nextInt();
+            System.out.println(finalPoints);
+
+            System.out.print("your grade in test category: ");
+            double testCatGrade = input.nextDouble();
+            System.out.println(testCatGrade);
+
+            System.out.print("desired grade: ");
+            double desiredGrade = input.nextDouble();
+            System.out.println(desiredGrade);
+
+            double finalScore = ((0.01*testCatGrade*finalPoints*testCat)-(finalPoints*currentGrade)+(finalPoints*desiredGrade)-(currentGrade*testCatPoints)+(testCatPoints*desiredGrade))/testCat;
+            System.out.print("You need to get at least a " + String.format("%.2f", (finalScore/finalPoints)*100) + "% on you final to get a " + desiredGrade);
         }
 
     }
